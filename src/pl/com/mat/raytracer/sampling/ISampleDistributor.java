6338b55466755759b7355ca16ec33fcf776d33d5
matixos <mati088@gmail.com>
2014-07-11 00:30:18 +0200

    Remove moved class files

--- src/pl/com/mat/raytracer/sampling/ISampleDistributor.java
+++
@@ -1,9 +0,0 @@
-package pl.com.mat.raytracer.sampling;
-
-import pl.com.mat.raytracer.utils.Vector2;
-
-public interface ISampleDistributor {
-
-	Vector2 mapSample(Vector2 sample);
-	
-}
--- src/pl/com/mat/raytracer/sampling/ISampleGenerator.java
+++
@@ -1,9 +0,0 @@
-package pl.com.mat.raytracer.sampling;
-
-import pl.com.mat.raytracer.utils.Vector2;
-
-public interface ISampleGenerator {
-	
-	Vector2[] sample(int count);
-
-}
